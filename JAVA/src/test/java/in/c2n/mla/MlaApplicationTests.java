b593f5d158fe15d3078bac9c039f1ad8378a94a0
Simon Aluvala <simon.a@c2n.in>
2018-05-10 11:52:55 +0530

    Initiated an application for services

---
+++ JAVA/src/test/java/in/c2n/mla/MlaApplicationTests.java
@@ -0,0 +1,16 @@
+package in.c2n.mla;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class MlaApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
